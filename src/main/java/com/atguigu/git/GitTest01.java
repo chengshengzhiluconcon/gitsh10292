9d2656b9464848506b0ba9cb3752379ca7712607
chengshengzhiluconcon <3358941163@qq.com>
2021-10-29 22:50:15 +0800

    four2 commit

---
+++ src/main/java/com/atguigu/git/GitTest01.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+        System.out.println("git test2");
+        System.out.println("git test3");
+        System.out.println("git test4");
+        System.out.println("git test5");
+        System.out.println("push test");
+        System.out.println("000");
+    }
+}
