4a9560e86f016d4793ccdd9c5310af51e0966136
chengshengzhiluconcon <91359310+chengshengzhiluconcon@users.noreply.github.com>
2021-10-29 23:09:54 +0800

    Update GitTest01.java

--- src/main/java/com/atguigu/git/GitTest01.java
+++ src/main/java/com/atguigu/git/GitTest01.java
@@ -10,6 +10,7 @@ public class GitTest01 {
         System.out.println("push test");
         System.out.println("push test1");
         System.out.println("push test2");
-
+        System.out.println("pull test");
+        System.out.println("pull test2");
     }
 }
